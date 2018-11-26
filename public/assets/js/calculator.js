46b21f06db3ae85c9a49fc6308ea0972d85f63c7
Ajat Darojat <ajatdarojat45@gmail.com>
2018-11-27 00:11:13 +0700

    add grid module

--- public/assets/js/calculator.js
+++ public/assets/js/calculator.js
@@ -6,7 +6,7 @@ async function loadMenuHTML() {
     await fetch('components/menu.html').then(response => response.text()).then(response => {
         menuElement.innerHTML = response;
 
-        let currentMenu = document.getElementsByClassName('menu')[0].getElementsByTagName('a')[1];
+        let currentMenu = document.getElementsByClassName('menu')[0].getElementsByTagName('a')[2];
         currentMenu.className = 'active';
     })
         .catch(error => console.log(error));
